36c152a6fe84dd62d4566d725563dc790df197c4
Šimon Raichl <raichls44@gmail.com>
2018-09-08 21:51:13 +0200

    just annotation added

--- app/src/main/java/com/VelociraptorCZE/HTML2Entities/Main.kt
+++ app/src/main/java/com/VelociraptorCZE/HTML2Entities/Main.kt
@@ -1,6 +1,7 @@
-/*
-*
-*
+/*HTML2Entities v1.2
+* Copyright (C) Simon Raichl 2018
+* MIT License
+* Use this as you want, share it as you want, do basically whatever you want with this :)
 */
 
 package com.VelociraptorCZE.HTML2Entities
